f6acd0f8e016566587823ab75f071d74b88eeba2
LisaCR01 <lisagunn01@gmail.com>
2022-06-11 16:06:06 +0100

    got signup and login routes and content to show

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,117 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
-import axios from "axios"
+// import axios from "axios"
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-// WE NEED TO ADD APOLLO BACK AFTER TEST!! :) import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 // Google Books API calls
-const getBooksByAuthor = 'https://www.googleapis.com/books/v1/volumes?q=prideandprejudice+intitle&key=AIzaSyDgmjmghFQvvxLztdDeOKE0eqkG_HgdV84'
+// const getBooksByAuthor = 'https://www.googleapis.com/books/v1/volumes?q=prideandprejudice+intitle&key=AIzaSyDgmjmghFQvvxLztdDeOKE0eqkG_HgdV84'
 
 
-// const client = new ApolloClient({
-//   uri: '/graphql',
-//   cache: new InMemoryCache(),
-// });
+ const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+ });
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 
 function App() {
-
-const [book, setBook] = useState("");
-const [result, setResult] = useState([]);
-const [apiKey, setApiKey] = useState("AIzaSyDgmjmghFQvvxLztdDeOKE0eqkG_HgdV84");
-
-function changeHandler(event){
-
-  const book = event.target.value;
-
-  setBook(book);
-}
-
-function submitHandler(event){
-  event.preventDefault();
-
-  axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&maxResult=10" + "&key=" + apiKey)
-  .then(data =>  {
-    console.log(data.data.items)
-    setResult(data.data.items)
-  })
-}
-
-
   return (
-
-   <div>
-    <h1>Search Books</h1>
-     <form onSubmit={submitHandler}>
-       <div>
-         <input type ="text" onChange={changeHandler} placeholder="Enter a book name" autoComplete="off"></input>;
-       </div>
-       <button type="submit"> Search
-       </button>
-     </form>
-
-     {result.map(book =>
-      <img src = {book.volumeInfo.imageLinks.thumbnail} alt ={book.title}
-      />
-     )}
-      </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+            {/* <Nav /> */}
+            <Routes>
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              {/* <Route 
+                path="/success" 
+                element={<Success />} 
+              /> */}
+              <Route 
+                // path="*" 
+                // element={<NoMatch />} 
+              />
+            </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
-
 export default App;
+
+
+// function App() {
+
+// const [book, setBook] = useState("");
+// const [result, setResult] = useState([]);
+// const [apiKey, setApiKey] = useState("AIzaSyDgmjmghFQvvxLztdDeOKE0eqkG_HgdV84");
+
+// function changeHandler(event){
+
+//   const book = event.target.value;
+
+//   setBook(book);
+// }
+
+// function submitHandler(event){
+//   event.preventDefault();
+
+//   axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&maxResult=10" + "&key=" + apiKey)
+//   .then(data =>  {
+//     console.log(data.data.items)
+//     setResult(data.data.items)
+//   })
+// }
+
+
+//   return (
+
+//    <div>
+//     <h1>Search Books</h1>
+//      <form onSubmit={submitHandler}>
+//        <div>
+//          <input type ="text" onChange={changeHandler} placeholder="Enter a book name" autoComplete="off"></input>;
+//        </div>
+//        <button type="submit"> Search
+//        </button>
+//      </form>
+
+//      {result.map(book =>
+//       <img src = {book.volumeInfo.imageLinks.thumbnail} alt ={book.title}
+//       />
+//      )}
+//       </div>
+//   );
+// }
+
+
