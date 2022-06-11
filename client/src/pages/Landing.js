79e348987cbe9a41fdc36710fafecd193813cd00
Allure <samira-h@hotmail.co.uk>
2022-06-11 17:26:11 +0100

    created landing page

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import axios from "axios"
+// import { Link } from 'react-router-dom';
+
+const Landing = () => {
+    return (
+      <div>
+        <p>hello</p>
+
+      </div>
+    );
+  };
+  
+  export default Landing;
