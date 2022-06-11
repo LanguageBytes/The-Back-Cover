b6f27b77ffa1d6edf6c4be4c6dcc399283418ba2
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-06-11 11:34:42 +0100

    Testing api call with react, working with totalItems example

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,32 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import React, { useState, useEffect } from "react";
 import './App.css';
 import axios from "axios"
+// WE NEED TO ADD APOLLO BACK AFTER TEST!! :) import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 // Google Books API calls
-const getBooksByTitle = 'https://www.googleapis.com/books/v1/volumes?q=search+intitle&key=AIzaSyDgmjmghFQvvxLztdDeOKE0eqkG_HgdV84'
-const getBooksByAuthor = 'https://www.googleapis.com/books/v1/volumes?q=search+intitle&key=AIzaSyDgmjmghFQvvxLztdDeOKE0eqkG_HgdV84'
+const getBooksByAuthor = 'https://www.googleapis.com/books/v1/volumes?q=prideandprejudice+intitle&key=AIzaSyDgmjmghFQvvxLztdDeOKE0eqkG_HgdV84'
 
 
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
-
+// const client = new ApolloClient({
+//   uri: '/graphql',
+//   cache: new InMemoryCache(),
+// });
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    getBooksWithAxios();
+  }, []);
+
+  const getBooksWithAxios = async () => {
+    const response = await axios.get(getBooksByAuthor);
+    setBooks(response.data);
+  };
+
   return (
-    <ApolloProvider client={client}>
-      <h1>hello</h1>
-    </ApolloProvider>
+   <div>
+      <p>hello{books.totalItems}</p>
+      </div>
   );
 }
 
