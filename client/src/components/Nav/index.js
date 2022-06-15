e97a892d16a4affec00830f0a47169a4ff3ab9b2
LisaCR01 <lisagunn01@gmail.com>
2022-06-15 11:30:54 +0100

    added basic styling to nav and is working

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,64 +1,63 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
-// import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-// import { useColorModeValue } from "@chakra-ui/color-mode";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import { useColorModeValue } from "@chakra-ui/color-mode";
 // import { Text } from '@chakra-ui/react'
+import { Heading } from '@chakra-ui/react'
 
 function Nav() {
 
-  // const colors = useColorModeValue(
-  //   ['green.50', 'teal.50', 'blue.50','purple.50','pink.50','orange.50'],
-  // )
-  // const [tabIndex, setTabIndex] = React.useState(0)
-  // const bg = colors[tabIndex]
+  const colors = useColorModeValue(
+    ['green.50', 'teal.50', 'blue.50','purple.50','pink.50','orange.50'],
+  )
+  const [tabIndex, setTabIndex] = React.useState(0)
+  const bg = colors[tabIndex]
 
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        // <Tabs onChange={(index) => setTabIndex(index)} bg={bg} colorScheme='white'>
-        //   <TabList>
-        //     <Header color='blue.600' as='i' fontSize='30px' p='2'></Header>
-        <ul >
-          {/* <Tab> */}
-          <li c>
+        <Tabs onChange={(index) => setTabIndex(index)} bg={bg} colorScheme='white'>
+          <TabList>
+            <Heading color='blue.600' as='i' fontSize='30px' p='2'></Heading>
+            <Tab  fontSize='20px' color='#48BB78'>
+          <div c>
             <Link to="/home">
               Discover
             </Link>
-          </li>
-          {/* </Tab>
-          <Tab> */}
-          <li c>
+          </div>
+          </Tab >
+          <Tab fontSize='20px' color='#38B2AC'>
+          <div c>
             <Link to="/mybooks">
               My Books
             </Link>
-          </li>
-          {/* </Tab>
-          <Tab> */}
-          <li c>
+          </div>
+          </Tab >
+          <Tab fontSize='20px' color='#4299E1'>
+          <div c>
             <Link to="/community">
               Community
             </Link>
-          </li>
-          {/* </Tab>
-          <Tab> */}
-          <li c>
+          </div>
+          </Tab>
+          <Tab fontSize='20px' color='#9F7AEA'>
+          <div c>
             <Link to="/contact">
               Contact 
             </Link>
-          </li>
-          {/* </Tab>
-          <Tab> */}
-          <li >
+          </div>
+          </Tab>
+          <Tab  fontSize='20px' color='#ED8936'>
+          <div >
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
             <a href="/" onClick={() => Auth.logout()}>
               Logout
             </a>
-          </li>
-          {/* </Tab> */}
-          </ul>
-          // </TabList>
-          // </Tabs>
+          </div>
+          </Tab>
+          </TabList>
+          </Tabs>
       );
     } else {
       return (
