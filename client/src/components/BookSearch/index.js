f6a6fcc0a2526e834ff53680ae298d2130ada101
LisaCR01 <lisagunn01@gmail.com>
2022-06-15 16:44:42 +0100

    started trying to make a wrap for the photos in discovery

--- client/src/components/BookSearch/index.js
+++ client/src/components/BookSearch/index.js
@@ -2,7 +2,13 @@ import React, {useState} from 'react';
 import './style.css';
 import axios from "axios"
 import Favourites from "../Favourites"
-
+import { Center, Container, FormControl } from '@chakra-ui/react'
+import {Heading} from  '@chakra-ui/react'
+import {Input} from  '@chakra-ui/react'
+import {Button} from  '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
+import {Box} from '@chakra-ui/react'
+import { Flex, Spacer } from '@chakra-ui/react'
 
 // // Google Books API calls
 // const getBooksByAuthor = 'https://www.googleapis.com/books/v1/volumes?q=prideandprejudice+intitle&key=AIzaSyDgmjmghFQvvxLztdDeOKE0eqkG_HgdV84'
@@ -33,23 +39,35 @@ function submitHandler(event){
 
   return (
 
-   <div>
-    <h1>Search Books</h1>
+   <Container>
+    <Center>
+      <Grid h='200px'
+            templateRows='repeat(2, 1fr)'
+            templateColumns='repeat(5, 1fr)'
+            gap={4}>
+    <GridItem rowSpan={1} colSpan={2}>
+    <FormControl  p='4' my='4' boxShadow='dark-lg' className="discovery-form-background" p='3'>
+    <Heading fontWeight="light" className="form-heading-discovery" py='2' fontSize='25px'>Search Books</Heading>
      <form onSubmit={submitHandler}>
-       <div>
-         <input type ="text" onChange={changeHandler} placeholder="Enter a book name" autoComplete="off"></input>;
-       </div>
-       <button type="submit"> Search
-       </button>
+         <Input bg='cyan.100' type ="text" onChange={changeHandler} placeholder="Enter a book name" autoComplete="off"></Input>
+       <Button  variant='solid' mt='4' className="discovery-button" type="submit"> Search
+       </Button>
      </form>
-
+     </FormControl>
+     </GridItem>
+     <GridItem rowSpan={4} colSpan={2}>
      {result.map(book =>
-     <>
+     <Flex>
+      <Box flex='1'>
       <img src = {book.volumeInfo.imageLinks.thumbnail} alt ={book.title}/>
-      <button data-bookTitle={book.id} data-bookCover={book.volumeInfo.imageLinks.thumbnail}> Favourite </button>
-     </>
+      <Button data-bookTitle={book.id} data-bookCover={book.volumeInfo.imageLinks.thumbnail}> Favourite </Button>
+      </Box>
+     </Flex>
      )}
-      </div> 
+     </GridItem>
+     </Grid>
+     </Center>
+      </Container> 
   );
 
      }
