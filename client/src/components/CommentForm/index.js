622d3b10ecbbdf71e42dcc96c1c033261870c30f
Allure <samira-h@hotmail.co.uk>
2022-06-16 22:40:21 +0100

    commentform function

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -6,3 +6,26 @@ import { ADD_COMMENT } from '../../utils/mutations';
 
 import Auth from '../../utils/auth';
 
+const CommentForm = ({ thoughtId }) => {
+  const [commentText, setCommentText] = useState('');
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const [addComment, { error }] = useMutation(ADD_COMMENT);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addComment({
+        variables: {
+          thoughtId,
+          commentText,
+          commentAuthor: Auth.getProfile().data.username,
+        },
+      });
+
+      setCommentText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
