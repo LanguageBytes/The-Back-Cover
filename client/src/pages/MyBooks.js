679879f0b7e77592b21b67b3240496d7bc315e6d
Sarah Lloyd <sarahlloyd407a@gmail.com>
2022-06-16 20:56:48 +0100

    Saved books now rendering to page

--- client/src/pages/MyBooks.js
+++ client/src/pages/MyBooks.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { useQuery } from '@apollo/client';
-import Favourites from '../components/Favourites';
-import { QUERY_BOOKS } from '../utils/queries';
+import { useQuery } from "@apollo/client";
+import Favourites from "../components/Favourites";
+import { QUERY_BOOKS } from "../utils/queries";
 
 const MyBooks = () => {
+  const { data } = useQuery(QUERY_BOOKS);
+  const books = data?.books || [];
 
-const { data } = useQuery(QUERY_BOOKS);
-const books = data?.book || [];
-
-    return (
-      <div>
-        <Favourites
-          books={books}/>
+  return (
+    <div>
+      <Favourites books={books} />
     </div>
-    );
-  };
-  
-  export default MyBooks;
+  );
+};
+
+export default MyBooks;
