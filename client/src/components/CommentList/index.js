ee0a1cbc5a3a6c5c3d393ee75747533a2a099bb2
Allure <samira-h@hotmail.co.uk>
2022-06-16 22:42:15 +0100

    return comment function

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -5,3 +5,11 @@ const CommentList = ({ comments = [] }) => {
     return <h3>No Comments Yet</h3>;
   }
 
+  return (
+    <>
+      <h3
+        className="p-5 display-inline-block"
+        style={{ borderBottom: '1px dotted #1a1a1a' }}
+      >
+        Comments
+      </h3>
