5104387470afff6e9348685cb57497080519dba4
Allure <samira-h@hotmail.co.uk>
2022-06-16 22:45:41 +0100

    made edits to mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -40,12 +40,7 @@ export const ADD_USER = gql`
 
  export const ADD_COMMENT = gql`
   mutation addComment($thoughtId: ID!, $commentText: String!) {
-    addComment(thoughtId: $thoughtId, commentText: $commentText) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
+    addComment(thoughtId: $thoughtId, commentText: $commentText)  {
         _id
         commentText
         createdAt
