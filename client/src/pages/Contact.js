6526bc456ab509ea5cb01a6a51ab9fc8f0636c0a
LisaCR01 <lisagunn01@gmail.com>
2022-06-16 13:28:24 +0100

    made contact responsive

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -17,7 +17,8 @@ const Contact = () => {
     return (
       <Container mt='4' p={{ base: '0', md: '4', lg: '8' }} boxShadow='dark-lg' className="contact-container">
         <Heading fontSize='30px ' p='4' fontWeight='normal' className="contact-title" >Contact Us</Heading>
-      <Center bg='orange.100'>
+    <Hide above='lg'>
+      <Center bg='orange.100' >
            <Grid templateColumns='repeat(4, 1fr)' gap={6}>
             <Hide below='md'>
               <GridItem w='100%' p='4'>
@@ -45,19 +46,73 @@ const Contact = () => {
                 </a>
                </GridItem>
            </Grid>
+        </Center>
+        </Hide>
+        <Hide below='lg'>
+        <Center bg='orange.100' p='4'>
+           <Grid templateColumns='repeat(5, 1fr)' gap={0}>
+                <GridItem  p='6' mt='2'>
+                <Text  color='orange.500' fontSize='24px'>Sarah</Text>
+               </GridItem> 
+              <GridItem p='4'>
+              <Avatar size='xl' name='Sarah' src={sarah} />
+               </GridItem> 
+               <GridItem  p='6'  mt='2' >
+                <a href="https://github.com/LanguageBytes">
+                <Icon color='orange.500' as={FiGithub} w='16'  h= '16' />
+                </a>
+               </GridItem>
+               <GridItem p='6' mt='2' >
+                <a href="https://www.linkedin.com/in/sarah-lloyd-19673b135/">
+                <Icon color='orange.500' as={FiLinkedin} w='16' h='16'/>
+                </a>
+               </GridItem>
+               <GridItem  p='6' mt='2'>
+                <a href="mailto:lcrgunn@gmail.com">
+                <Icon color='orange.500' as={FiMail} w= '16' h='16'/>
+                </a>
+               </GridItem>
+           </Grid>
        </Center>
+       </Hide>
        <Center bg='red.100'>
+        <Hide above='lg'>
        <Grid templateColumns='repeat(4, 1fr)' gap={6}>
             <Hide below='md'>
+              <GridItem p='4'>
+              <Avatar size='xl' name='Lisa' src={lisa} />
+               </GridItem> 
+            </Hide>
+            <Hide above='md'>
+               <GridItem p='6' mt='2' mx={{base:'2'}}>
+                <Text color='red.500' fontSize='22px'>Lisa</Text>
+               </GridItem> 
+            </Hide>
+               <GridItem  p='6' mt='2'  >
+                <a href="https://github.com/LisaCR01">
+                <Icon color='red.500'  as={FiGithub} w={{ base: '8', md: '12', lg: '16' }} h={{ base: '8', md: '12', lg: '16' }} />
+                </a>
+               </GridItem>
+               <GridItem w='100%' p='6' mt='2' >
+                <a href="https://www.linkedin.com/in/LisaCR01">
+                <Icon color='red.500' as={FiLinkedin} w={{ base: '8', md: '12', lg: '16' }} h={{ base: '8', md: '12', lg: '16' }} />
+                </a>
+               </GridItem>
+               <GridItem w='100%' p='6' mt='2'>
+                <a href="mailto:lcrgunn@gmail.com">
+                <Icon color='red.500' as={FiMail} w={{ base: '8', md: '12', lg: '16' }} h={{ base: '8', md: '12', lg: '16' }} />
+                </a>
+               </GridItem>
+           </Grid>
+           </Hide>
+           <Hide below='lg'>
+            <Grid templateColumns='repeat(5, 1fr)' gap={0}>
+                <GridItem w='100%' p='6' mt='2'>
+                <Text px='3' color='red.500' fontSize='24px'>Lisa</Text>
+               </GridItem> 
               <GridItem w='100%' p='4'>
               <Avatar size='xl' name='Lisa' src={lisa} />
                </GridItem> 
-               </Hide>
-               <Hide above='md' >
-               <GridItem w='100%' p='6' mt='2' mx={{base:'2'}}>
-                <Text color='red.500' fontSize='22px'>Lisa</Text>
-               </GridItem> 
-               </Hide>
                <GridItem w='100%' p='6' mt='2'  >
                 <a href="https://github.com/LisaCR01">
                 <Icon color='red.500'  as={FiGithub} w={{ base: '8', md: '12', lg: '16' }} h={{ base: '8', md: '12', lg: '16' }} />
@@ -74,8 +129,10 @@ const Contact = () => {
                 </a>
                </GridItem>
            </Grid>
+           </Hide>
        </Center>
        <Center bg='blue.100'>
+       <Hide above='lg'>
        <Grid templateColumns='repeat(4, 1fr)' gap={6}>
              <Hide  below='md'>
               <GridItem w='100%' p='4'>
@@ -103,6 +160,32 @@ const Contact = () => {
                 </a>
                </GridItem>
            </Grid>
+        </Hide>
+        <Hide  below='lg'>
+        <Grid templateColumns='repeat(5, 1fr)' gap={0}>
+             <GridItem  w='100%' p='6' mt='2'>
+                <Text color='blue.500' fontSize='24px'>Samira</Text>
+               </GridItem>
+              <GridItem w='100%' p='4'>
+              <Avatar size='xl' name='Samira' src={samira} />
+               </GridItem>  
+               <GridItem w='100%' p='6' mt='2'>
+                <a href="https://github.com/samira0101">
+                <Icon color='blue.500' as={FiGithub} w={{ base: '8', md: '12', lg: '16' }} h={{ base: '8', md: '12', lg: '16' }} />
+                </a>
+               </GridItem>
+               <GridItem w='100%' p='6' mt='2'>
+                <a href="www.linkedin.com/in/samira-hirsi-4609131a8">
+                <Icon color='blue.500' as={FiLinkedin} w={{ base: '8', md: '12', lg: '16' }} h={{ base: '8', md: '12', lg: '16' }} />
+                </a>
+               </GridItem>
+               <GridItem w='100%' p='6' mt='2'>
+                <a  href="mailto:lcrgunn@gmail.com">
+                <Icon color='blue.500' as={FiMail} w={{ base: '8', md: '12', lg: '16' }} h={{ base: '8', md: '12', lg: '16' }} />
+                </a>
+               </GridItem>
+           </Grid>
+        </Hide>
        </Center>
        </Container>
     );
