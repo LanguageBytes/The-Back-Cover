fda55ba000419fb51bab0a5e7ac7009665ff459b
LisaCR01 <lisagunn01@gmail.com>
2022-06-16 14:39:33 +0100

    made background size responsive using media queries

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -10,10 +10,11 @@ import {
     useColorModeValue,
   } from '@chakra-ui/react';
   import { ReactNode } from 'react';
+  import './style.css'
   
   const Footer = () => { 
     return (
-        <div>
+        <div className="footer">
       <Text bg='orange.100' heigh='100%' pt={6} fontSize={'sm'} textAlign={'center'}>
         © 2022 Chakra Templates. All rights reserved
       </Text>
