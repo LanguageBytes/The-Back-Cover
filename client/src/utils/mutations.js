d644704ab9ab45f7033a1ab2d5735fa77387b89a
LisaCR01 <lisagunn01@gmail.com>
2022-06-16 23:37:42 +0100

    added sarh seeder

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -38,14 +38,14 @@ export const ADD_USER = gql`
  }
  `;
 
- export const ADD_COMMENT = gql`
-  mutation addComment($thoughtId: ID!, $commentText: String!) {
-    addComment(thoughtId: $thoughtId, commentText: $commentText)  {
-        _id
-        commentText
-        createdAt
-      }
-    }
-  }
-`;
+//  export const ADD_COMMENT = gql`
+//   mutation addComment($thoughtId: ID!, $commentText: String!) {
+//     addComment(thoughtId: $thoughtId, commentText: $commentText)  {
+//         _id
+//         commentText
+//         createdAt
+//       }
+//     }
+//   }
+// `;
 
